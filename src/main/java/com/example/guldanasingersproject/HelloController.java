9d7d2f96d67681a6efb3e52984d2be17280cdddc
Alexander Zharkov <alexander.06.zharkov@gmail.com>
2021-09-12 21:47:17 +0600

    Feature. Error window + checking presence of provided login/password

--- src/main/java/com/example/guldanasingersproject/HelloController.java
+++ src/main/java/com/example/guldanasingersproject/HelloController.java
@@ -27,15 +27,15 @@ public class HelloController {
         DatabaseConnection dbConnection = new DatabaseConnection();
         Connection dbLink = dbConnection.getDatabaseLink();
         String login = loginField.getText();
-        String password = passwordField.getText();
+            String password = passwordField.getText();
 
-        if (login.isEmpty()) {
-            new AlertWindow().showWindow("Вход в систему", "Вы не ввели логин");
-            return;
-        }
+            if (login.isEmpty()) {
+                new AlertWindow().showWarningWindow("Вход в систему", "Вы не ввели логин");
+                return;
+            }
 
-        if (password.isEmpty()) {
-            new AlertWindow().showWindow("Вход в систему", "Вы не ввели пароль");
+            if (password.isEmpty()) {
+            new AlertWindow().showWarningWindow("Вход в систему", "Вы не ввели пароль");
             return;
         }
 
@@ -44,7 +44,15 @@ public class HelloController {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Users");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("name"));
+                String actualLogin = resultSet.getString("login");
+                String actualPassword = resultSet.getString("password");
+
+                if (actualLogin.equals(login) && actualPassword.equals(password)) {
+                    System.out.println("Мы узнали вас");
+                    return;
+                }
+                new AlertWindow().showErrorWindow("Вход в систему", "Вы ввели неправильный логин или пароль. Проверьте ввод и попробуйте еще раз");
+
             }
         } catch (Exception error) {
             error.printStackTrace();
@@ -55,7 +63,7 @@ public class HelloController {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Подтвердите действие");
         alert.setHeaderText("Закрытие приложения");
-        alert.setContentText("Вы уверены, что ходите закрыть прилоежние?");
+        alert.setContentText("Вы уверены, что ходите закрыть приложение?");
 
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK){
--- src/main/java/com/example/guldanasingersproject/Windows/AlertWindow.java
+++ src/main/java/com/example/guldanasingersproject/Windows/AlertWindow.java
@@ -6,9 +6,17 @@ import javafx.scene.control.ButtonType;
 import java.util.Optional;
 
 public class AlertWindow {
-    public void showWindow(String header, String message) {
+    public void showWarningWindow(String header, String message) {
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Предупреждение");
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+        Optional<ButtonType> result = alert.showAndWait();
+    }
+
+    public void showErrorWindow(String header, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Ошибка");
         alert.setHeaderText(header);
         alert.setContentText(message);
         Optional<ButtonType> result = alert.showAndWait();
