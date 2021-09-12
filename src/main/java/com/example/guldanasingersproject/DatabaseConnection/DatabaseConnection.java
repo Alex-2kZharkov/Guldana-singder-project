80fdf82e9a45e3beb25e2f6e3b40024333277984
Alexander Zharkov <alexander.06.zharkov@gmail.com>
2021-09-12 12:36:59 +0600

    Initial commit

---
+++ src/main/java/com/example/guldanasingersproject/DatabaseConnection/DatabaseConnection.java
@@ -0,0 +1,13 @@
+package com.example.guldanasingersproject.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.Driver;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public Connection getDatabaseLink() {
+        String databaseName = "";
+//        String data
+    }
+}
---
+++ src/main/java/com/example/guldanasingersproject/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.guldanasingersproject;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application implements EventHandler<ActionEvent> {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 438, 392);
+        stage.setTitle("Музыкальное приложение");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
